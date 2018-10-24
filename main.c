911555be1ee034e01d4b47e0f9e2cc8ff79ee693
Monica Galvao <mksgalvao@gmail.com>
2018-10-24 15:44:18 -0300

    time and swaps counters done

--- main.c
+++ main.c
@@ -26,7 +26,7 @@ void mostrarVetor(int *vetor, int tam)
 
 }
 
-int swap(int arr[], int i, int j)
+int swap(int *arr, int i, int j)
 {
     int troca = 0;
     int temp = arr[i];
@@ -37,17 +37,19 @@ int swap(int arr[], int i, int j)
 
     return troca;
 }
+
+
 int bolha(int *vetor, int tam)
 {
-    int troca;
+    int troca = 0;
     // Base case
     if (tam == 1)
 
-        return;
+        return 0;
 
     for (int i=0; i<tam-1; i++)
         if (vetor[i] > vetor[i+1])
-            troca =  swap(vetor, i, i+1);
+            troca +=  swap(vetor, i, i+1);
 
     bolha(vetor, tam-1);
 
@@ -57,73 +59,76 @@ int bolha(int *vetor, int tam)
 
 
 // Recursive function to perform selection sort on subarray arr[i..n-1]
-int selecao(int arr[], int i, int n)
+int selecao(int *vetor, int i, int tam, int troca)
 {
-    // find the minimum element in the unsorted subarray[i..n-1]
-    // and swap it with arr[i]
-    int troca = 0;
-    int min = i;
-    for (int j = i + 1; j < n; j++)
+
+   for (int i = 0; i < tam - 1; i++)
+    {
+        // find the minimum element in the unsorted subarray[i..n-1]
+        // and swap it with arr[i]
+        int min = i;
+
+    for (int j = i + 1; j < tam; j++)
     {
         // if arr[j] element is less, then it is the new minimum
-        if (arr[j] < arr[min])
+        if (vetor[j] < vetor[min])
             min = j;    // update index of min element
     }
 
     // swap the minimum element in subarray[i..n-1] with arr[i]
-    troca = swap(arr, min, i);
-
-    if (i + 1 < n)
+      troca += swap(vetor, min, i);
+}
+    if (i + 1 < tam)
     {
-        selecao(arr, i + 1, n);
+        troca += selecao(vetor, i + 1, tam, troca);
     }
+
     return troca;
 }
 
 
 
-void quick(int *arr, int low, int high)
-{
-    int pivot, i, j, temp;
-    if(low < high)
-    {
-        pivot = low; // select a pivot element
-        i = low;
-        j = high;
-        while(i < j)
-        {
-            // increment i till you get a number greater than the pivot element
-            while(arr[i] <= arr[pivot] && i <= high)
-                i++;
-            // decrement j till you get a number less than the pivot element
-            while(arr[j] > arr[pivot] && j >= low)
-                j--;
-            // if i < j swap the elements in locations i and j
-            if(i < j)
-            {
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-        // when i >= j it means the j-th position is the correct position
-        // of the pivot element, hence swap the pivot element with the
-        // element in the j-th position
-        temp = arr[j];
-        arr[j] = arr[pivot];
-        arr[pivot] = temp;
-        // Repeat quicksort for the two sub-arrays, one to the left of j
-        // and one to the right of j
-        quick(arr, low, j-1);
-        quick(arr, j+1, high);
-    }
-}
+int quick(int *vetor, int inicio, int fim, int troca){
+
+	int i, j, pivo, aux;
+
+	i = inicio;
+	j = fim;
+	troca = 0;
+
+	pivo = vetor[(inicio+fim)/2];
+	do{
+		while(vetor[i]<pivo)
+		i++; // enquanto o valor do vetor na posição i for menor que o pivô  o -i avança para direita
+		while(vetor[j]>pivo)
+		j--;// enquanto o valor do vetor na posição j for maior que o pivô  o -i retrocede para esquerda
+		if(i<=j) // se a posição de i for menor ou igual a j (I a esquerda e j a direita ou encontro de i e j)
+		{
+			aux = vetor[i];
+			vetor[i]=vetor[j];
+			vetor[j] = aux; // troca
+
+			i++;
+			j--;
+			troca ++;
+	}
+	}while (i<=j); // laço roda até o encontro dos indices i e j
+
+	if(inicio <j)
+	troca += quick(vetor, inicio,j,troca); // se o incio for menor que o j chamar a função quick para a sub lista a esquerda
+
+	if(i<fim)
+	troca += quick(vetor, i, fim, troca); // se o i for menor que o fim chamar a função quik para a sublista a direita
+
+	return troca;
+	}
 
 int main()
 {
 
     int* vetor;
     int inicio, fim, tempo, tempo1, troca;
+    troca =0;
     int opc, tam;
 
 
@@ -153,8 +158,8 @@ int main()
         tempo1 = tempo/1000;
         printf(" \n Vetor ordenado pelo metodo bublle sort eh: \n");
         mostrarVetor(vetor, tam);
-        printf("\n Tempo de ordemnacao: %d milisegundos \n", tempo);
-        printf("\n Tempo de ordemnacao: %d segundos \n", tempo1);
+        printf("\n Tempo de ordenacao: %d milisegundos \n", tempo);
+        printf("\n Tempo de ordenacao: %d segundos \n", tempo1);
         printf("\n Numero de trocas sao: %d \n", troca);
         system("pause");
         break;
@@ -162,14 +167,14 @@ int main()
         inicio = 0;
         troca = 0;
         inicio = GetTickCount();
-        troca = selecao(vetor,0, tam);
+        troca = selecao(vetor,0, tam, troca);
         fim = GetTickCount();
         tempo = fim - inicio;
         tempo1 = tempo/1000;
         printf(" \n Vetor ordenado pelo metodo selection sort eh: \n");
         mostrarVetor(vetor, tam);
-        printf("\n Tempo de ordemnacao: %d milisegundos \n", tempo);
-        printf("\n Tempo de ordemnacao: %d segundos \n", tempo1);
+        printf("\n Tempo de ordenacao: %d milisegundos \n", tempo);
+        printf("\n Tempo de ordenacao: %d segundos \n", tempo1);
         printf("\n Numero de trocas sao: %d \n", troca);
         system("pause");
         break;
@@ -177,14 +182,15 @@ int main()
         inicio = 0;
 
         inicio = GetTickCount();
-        quick(vetor, 0, tam-1);
+        troca = quick(vetor, 0, tam-1, troca);
         fim = GetTickCount();
         tempo = fim - inicio;
         tempo1 = tempo/1000;
         printf(" \n Vetor ordenado pelo metodo quick sort é: \n");
         mostrarVetor(vetor, tam);
-        printf("\n Tempo de ordemnacao: %d milisegundos \n", tempo);
-        printf("\n Tempo de ordemnacao: %d segundos \n", tempo1);
+        printf("\n Tempo de ordenacao: %d milisegundos \n", tempo);
+        printf("\n Tempo de ordenacao: %d segundos \n", tempo1);
+        printf("\n Numero de trocas sao: %d \n", troca);
         system("pause");
         break;
     default :
